9380e04f9ac08957260b0e00517abe5fd7618f71
jsun <jaysun054@gmail.com>
2021-10-08 21:18:40 -0700

    require

---
+++ index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generate;
+
+const Manager = require('./lib/Manager.js');
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
+
